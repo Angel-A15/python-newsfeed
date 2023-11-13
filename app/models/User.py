72a62c5df47b2de0bbde00c903cd2ba3a84578ba
Angel-A15 <aasupertwix15@gmail.com>
2023-11-13 13:57:44 -0600

    working on setting up user logins and sessions, in app.py, issue with signing up, unable to sign up

--- app/models/User.py
+++ app/models/User.py
@@ -20,6 +20,7 @@ class User(Base):
         assert '@' in email
 
         return email
+        
     @validates('password')
     def validate_password(self, key, password):
         assert len(password) > 4
